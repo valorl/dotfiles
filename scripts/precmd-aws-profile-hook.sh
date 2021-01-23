4ece22c882506288cd2f4af5ff628e5a146828c8
valorl <valer.orlovsky@gmail.com>
2021-01-23 23:40:54 +0100

    Make AWS_PROFILE precmd ignore terraform env dirs

--- scripts/precmd-aws-profile-hook.sh
+++ scripts/precmd-aws-profile-hook.sh
@@ -1,3 +1,4 @@
+[[ "${PWD}" == *"terraform/envs"* ]] && return 0;
 [[ "$VALORL_AWS_PROFILE_HOOK_SUSPEND" == "1" ]] && return 0
 ctx=$(sed -n "s/current-context: \(.*\)/\1/p" ~/.kube/config)
 case $ctx  in
