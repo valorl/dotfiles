97974ae96d33601c61ade59be89ddb240ed92857
valorl <valer.orlovsky@gmail.com>
2021-04-15 12:40:42 +0200

    Add terraform aliases module

---
+++ config/aliases/terraform.zsh
@@ -0,0 +1,22 @@
+alias tf=_terraform
+alias tfi="tf init"
+alias tfp=_terraform_plan
+
+alias tfip="tf init && tf plan"
+
+function _terraform() {
+  binPath="$HOME/.local/bin/terraform"
+
+  if command -v tfswitch &> /dev/null
+  then
+    tfswitch -b "$binPath" # > /dev/null
+    eval "$binPath $@"
+  else
+    terraform $@
+  fi
+}
+
+function _terraform_plan() {
+  _terraform plan -out=plan.tfplan $@
+}
+
