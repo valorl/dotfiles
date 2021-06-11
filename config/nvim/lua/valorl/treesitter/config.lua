8df0729fdfefab3d5f64a968e19d2cb5277c49df
valorl <valer.orlovsky@gmail.com>
2021-06-11 16:35:49 +0200

    add static treesitter config

--- config/nvim/lua/valorl/treesitter/config.lua
+++ config/nvim/lua/valorl/treesitter/config.lua
@@ -1,5 +1,9 @@
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "go", "c_sharp", "yaml" },
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
+  indent = {
+    enable = true,
+  }
 }
