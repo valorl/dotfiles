d1cc1bbf508996acfc0c01ba352c60aeda011509
valorl <11498571+valorl@users.noreply.github.com>
2021-10-31 22:00:01 +0100

    yamlls lsp messing around

--- config/nvim/lua/valorl/lsp/config.lua
+++ config/nvim/lua/valorl/lsp/config.lua
@@ -1,7 +1,49 @@
 require'lspconfig'.gopls.setup{}
 require'lspconfig'.terraformls.setup{}
-require'lspconfig'.yamlls.setup{}
 require'lspconfig'.bashls.setup{}
+require'lspconfig'.tsserver.setup{}
+
+local yamlls_schemas = (function ()
+  local schemas = {}
+  local mappings = {
+    ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = {
+      "kube/ci"
+    }
+  }
+
+  local cwd = vim.fn.getcwd()
+
+  for url, regexes in pairs(mappings) do
+    for _, r in ipairs(regexes) do
+      if vim.regex(r):match_str(cwd) then
+        if schemas[url] == nil then
+          schemas[url] = "/*.yaml"
+        end
+      end
+    end
+  end
+
+  return schemas
+end)()
+
+
+vim.lsp.set_log_level("trace")
+
+require'lspconfig'.yamlls.setup{
+  autostart = true,
+  settings = {
+    yaml = {
+      trace = {
+        server = "verbose"
+      },
+      -- schemas = yamlls_schemas,
+      -- schemas = { kubernetes = "/*.yaml" },
+      -- schemaStore = { enable = true},
+      schemaDownload = { enable = true },
+      validate = true,
+    }
+  }
+}
 
 local pid = vim.fn.getpid()
 local omnisharp_bin = "/home/vao/.local/omnisharp/run"
