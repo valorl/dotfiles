6dc5dd17d778c77638d0583c122a1fab27faa065
valorl <11498571+valorl@users.noreply.github.com>
2022-04-21 17:42:23 +0200

    fix(nvim): remove astronauta

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,6 +1,3 @@
--- https://github.com/tjdevries/astronauta.nvim/issues/1#issuecomment-896130539
-vim.cmd [[runtime plugin/astronauta.vim]]
-
 require("valorl.globals")
 require("valorl.lsp")
 require("valorl.dap")
@@ -8,7 +5,4 @@ require("valorl.compe")
 require("valorl.telescope")
 require("valorl.treesitter")
 require("valorl.indent-blankline")
-
-
-
 require("valorl.formatter")
