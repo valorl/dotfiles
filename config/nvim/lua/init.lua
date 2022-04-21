b24d2921979e754d283871b766f674be038e706d
valorl <11498571+valorl@users.noreply.github.com>
2022-04-21 17:20:39 +0200

    feat(nvim): add global utils

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,6 +1,7 @@
 -- https://github.com/tjdevries/astronauta.nvim/issues/1#issuecomment-896130539
 vim.cmd [[runtime plugin/astronauta.vim]]
 
+require("valorl.globals")
 require("valorl.lsp")
 require("valorl.dap")
 require("valorl.compe")
