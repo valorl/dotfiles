6fb0257a530939b490313afcbc13f1c9e6aa4c07
valorl <11498571+valorl@users.noreply.github.com>
2022-04-21 17:27:50 +0200

    feat(bin): support creds+config profiles in awp

--- scripts/source-aws-profile.sh
+++ scripts/source-aws-profile.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-profile=$(cat ~/.aws/credentials | sed -n 's/^\[\(.*\)\]$/\1/p' | fzf +m)
-export AWS_PROFILE=$profile
+#profile=$(cat ~/.aws/credentials | sed -n 's/^\[\(.*\)\]$/\1/p' | fzf +m)
+function profiles() {
+    cat ~/.aws/config | sed -n 's/^\[profile \(.*\)\]$/\1/p'
+    cat ~/.aws/credentials | sed -n 's/^\[\(.*\)\]$/\1/p'
+}
+profile=$( profiles | grep -v -E 'default|_login' | fzf +m)
 
+export AWS_PROFILE=$profile
