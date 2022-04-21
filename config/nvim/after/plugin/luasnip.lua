71fe2294df548dfaf3325be3595bd9cb7c3823aa
valorl <11498571+valorl@users.noreply.github.com>
2022-04-21 17:26:27 +0200

    temp: luasnip

---
+++ config/nvim/after/plugin/luasnip.lua
@@ -0,0 +1,140 @@
+print("after/plugin/luasnip.lua")
+
+-- if vim.g.snippets ~= "luasnip" then
+--   return
+-- end
+
+local ls = require "luasnip"
+local types = require "luasnip.util.types"
+
+ls.config.set_config {
+  -- This tells LuaSnip to remember to keep around the last snippet.
+  -- You can jump back into it even if you move outside of the selection
+  history = true,
+
+  -- This one is cool cause if you have dynamic snippets, it updates as you type!
+  updateevents = "TextChanged,TextChangedI",
+
+  -- Autosnippets:
+  enable_autosnippets = true,
+
+  -- Crazy highlights!!
+  -- #vid3
+  -- ext_opts = nil,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "<-", "Error" } },
+      },
+    },
+  },
+}
+
+-- create snippet
+-- s(context, nodes, condition, ...)
+local snippet = ls.s
+
+-- TODO: Write about this.
+--  Useful for dynamic nodes and choice nodes
+local snippet_from_nodes = ls.sn
+
+-- This is the simplest node.
+--  Creates a new text node. Places cursor after node by default.
+--  t { "this will be inserted" }
+--
+--  Multiple lines are by passing a table of strings.
+--  t { "line 1", "line 2" }
+local t = ls.text_node
+
+-- Insert Node
+--  Creates a location for the cursor to jump to.
+--      Possible options to jump to are 1 - N
+--      If you use 0, that's the final place to jump to.
+--
+--  To create placeholder text, pass it as the second argument
+--      i(2, "this is placeholder text")
+local i = ls.insert_node
+
+-- Function Node
+--  Takes a function that returns text
+local f = ls.function_node
+
+-- This a choice snippet. You can move through with <c-l> (in my config)
+--   c(1, { t {"hello"}, t {"world"}, }),
+--
+-- The first argument is the jump position
+-- The second argument is a table of possible nodes.
+--  Note, one thing that's nice is you don't have to include
+--  the jump position for nodes that normally require one (can be nil)
+local c = ls.choice_node
+
+local d = ls.dynamic_node
+
+-- TODO: Document what I've learned about lambda
+local l = require("luasnip.extras").lambda
+
+local events = require "luasnip.util.events"
+
+-- local str_snip = function(trig, expanded)
+--   return ls.parser.parse_snippet({ trig = trig }, expanded)
+-- end
+
+local shortcut = function(val)
+  if type(val) == "string" then
+    return { t { val }, i(0) }
+  end
+
+  if type(val) == "table" then
+    for k, v in ipairs(val) do
+      if type(v) == "string" then
+        val[k] = t { v }
+      end
+    end
+  end
+
+  return val
+end
+
+local make = function(tbl)
+  local result = {}
+  for k, v in pairs(tbl) do
+    table.insert(result, (snippet({ trig = k, desc = v.desc }, shortcut(v))))
+  end
+
+  return result
+end
+
+local snippets = {}
+
+-- `all` key means for all filetypes.
+-- Shared between all filetypes. Has lower priority than a particular ft tho
+
+-- initial text :: this is going to be replaced :: initial tthis is going to be replacedxt
+-- this is where we have text :: TEXT :: this is where we have TEXT
+
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/valorl/snips/ft/*.lua", true)) do
+  local ft = vim.fn.fnamemodify(ft_path, ":t:r")
+  snippets[ft] = make(loadfile(ft_path)())
+end
+
+ls.snippets = snippets
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
+
+vim.keymap.set("i", "<c-l>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end)
+
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
