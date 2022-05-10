477e06b100aa1c66e5c3712a24910e902d64a5cd
valorl <11498571+valorl@users.noreply.github.com>
2022-05-10 12:47:03 +0200

    feat(alias/k9s): abstract away bin location

--- config/aliases/k9s.zsh
+++ config/aliases/k9s.zsh
@@ -1,19 +1,23 @@
 function k9s() {
 
   noProfile=false
-  if [ "$1" = "--no-profile" ]
-  then
+  if [ "$1" = "--no-profile" ]; then
     noProfile=true
     shift
   fi
 
+  local k9s_bin=""
+
+  for p in "$HOME/.local/bin/k9s" "/usr/local/bin/k9s" "/usr/bin/k9s" "/bin/k9s"; do
+      [ -f "$p" ] && { k9s_bin="$p"; break }
+  done
 
   if [ $# -gt 0 ]
   then
-    /usr/local/bin/k9s $@
+    $k9s_bin $@
   else
     context=$(kubectl config get-contexts --no-headers -o name | fzf -1 +m)
-    cmd=( /usr/local/bin/k9s --context $context --command node)
+    cmd=( $k9s_bin --context $context --command node)
     profile=""
 
     if [ "$noProfile" = "false" ]
