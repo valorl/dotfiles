c7a0a8f4ab7d521839ca0e9f53aa6881f753b8fe
valorl <11498571+valorl@users.noreply.github.com>
2022-06-12 21:20:55 +0200

    feat(nvim): add CUE treesitter support

--- config/nvim/lua/valorl/treesitter/config.lua
+++ config/nvim/lua/valorl/treesitter/config.lua
@@ -1,3 +1,18 @@
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+        pattern = {"*.cue"},
+        command = "set filetype=cue",
+})
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.cue = {
+  install_info = {
+    url = "https://github.com/eonpatapon/tree-sitter-cue",
+    files = {"src/parser.c", "src/scanner.c"},
+    branch = "main"
+  },
+  filetype = "cue",
+}
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
   highlight = {
