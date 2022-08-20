8e5057d53ca53054a390adad6bc608a383722fe1
valorl <11498571+valorl@users.noreply.github.com>
2022-08-20 19:52:30 +0200

    refactor(nvim): rename things in telescope maps

--- config/nvim/lua/valorl/telescope/mappings.lua
+++ config/nvim/lua/valorl/telescope/mappings.lua
@@ -1,7 +1,7 @@
-local telescope = require('telescope.builtin')
+local builtin = require('telescope.builtin')
 
-vim.keymap.set("n", "<C-p>", telescope.git_files, { noremap = true })
-vim.keymap.set("n", "<leader>fc", require('telescope.command').load_command, { noremap = true })
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { noremap = true })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { noremap = true })
+vim.keymap.set("n", "<C-p>", function() builtin.git_files({show_untracked = true}) end, { noremap = true })
+vim.keymap.set("n", "<leader>fc", builtin.builtin, { noremap = true })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true })
 vim.keymap.set("n", "<leader>fb", require("telescope").extensions.file_browser.file_browser, { noremap = true })
