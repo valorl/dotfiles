bd31153bc2ed629e954d3b3027c5a9f1f81fa1fc
valorl <11498571+valorl@users.noreply.github.com>
2024-08-12 14:49:24 +0200

    chore(nvim): bunch of updates :shrug:

--- home/.config/nvim/lua/valorl/lazy/init.lua
+++ home/.config/nvim/lua/valorl/lazy/init.lua
@@ -4,6 +4,33 @@ return {
   "tpope/vim-fugitive",
   "rhysd/git-messenger.vim",
 
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      highlight = {
+        -- TODO: test
+        pattern = [[.*<(KEYWORDS).*:]]
+      }
+    }
+  },
+
+  {
+    "ruifm/gitlinker.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("gitlinker").setup({
+        callbacks = {
+          ["gitlab.neasenergy.com"] = require "gitlinker.hosts".get_gitlab_type_url
+        }
+      })
+    end
+  },
+
 
   {
     "numToStr/Comment.nvim",
@@ -18,4 +45,29 @@ return {
   -- fun
   -- "github/copilot.vim",
   -- "eandrju/cellular-automaton.nvim",
+  "github/copilot.vim",
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
+  },
+
 }
--- home/.config/nvim/lua/valorl/lazy/lsp.lua
+++ home/.config/nvim/lua/valorl/lazy/lsp.lua
@@ -45,6 +45,9 @@ return {
     "saadparwaiz1/cmp_luasnip",
 
     "j-hui/fidget.nvim",
+    "aznhe21/actions-preview.nvim",
+
+    "seblj/roslyn.nvim",
   },
 
   config = function()
@@ -66,7 +69,8 @@ return {
         vim.keymap.set("n", "<leader>grr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<leader>grn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>gca", vim.lsp.buf.code_action, opts)
+        -- vim.keymap.set("n", "<leader>gca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>gca", require("actions-preview").code_actions, opts)
         vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
         vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
         vim.keymap.set("n", "<leader>dh", vim.diagnostic.open_float, opts)
@@ -119,9 +123,10 @@ return {
                 staticcheck = true,
               }
             },
-            init_options = {
-              usePlaceholders = true
-            }
+            -- TODO: How to navigate these when they appear ?
+            -- init_options = {
+            --   usePlaceholders = true
+            -- }
           }
         end,
 
@@ -157,6 +162,13 @@ return {
             }
           }
         end,
+
+        ["tflint"] = function()
+          require("lspconfig").tflint.setup {
+            capabilities = capabilities,
+            cmd = { "tflint", "--langserver", "--disable-rule=terraform_required_providers" }
+          }
+        end
       }
     })
 
@@ -198,5 +210,49 @@ return {
       vim.lsp.handlers.hover,
       { border = "single" }
     )
+
+    -- roslyn.nvim
+    require("roslyn").setup({
+      config = {
+        -- Here you can pass in any options that that you would like to pass to `vim.lsp.start`
+        -- The only options that I explicitly override are, which means won't have any effect of setting here are:
+        --     - `name`
+        --     - `cmd`
+        --     - `root_dir`
+        --
+        --     - `on_init`
+        settings = {
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+        },
+      },
+      exe = {
+        "dotnet",
+        vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+      },
+      -- NOTE: Set `filewatching` to false if you experience performance problems.
+      -- Defaults to true, since turning it off is a hack.
+      -- If you notice that the server is _super_ slow, it is probably because of file watching
+      -- I noticed that neovim became super unresponsive on some large codebases, and that was because
+      -- it schedules the file watching on the event loop.
+      -- This issue went away by disabling that capability. However, roslyn will fallback to its own
+      -- file watching, which can make the server super slow to initialize.
+      -- Setting this option to false will indicate to the server that neovim will do the file watching.
+      -- However, in `hacks.lua` I will also just don't start off any watchers, which seems to make the server
+      -- a lot faster to initialize.
+      filewatching = true,
+    })
   end
 }
