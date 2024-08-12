7e97dedbc255fc92cf8a404603e6fbcef26fb890
valorl <11498571+valorl@users.noreply.github.com>
2024-08-12 14:51:05 +0200

    feat(alias): add more utils (gcp/kube/git/tofu)

--- home/.config/aliases/aliases.zsh
+++ home/.config/aliases/aliases.zsh
@@ -7,11 +7,20 @@ alias rm="rm -v"
 alias mkd="mkdir -pv"
 
 alias vimy="vim -c 'set ft=yaml'"
+alias vimj="vim -c 'set ft=json' -c '%!jq'"
 
 # Colors
-# alias ls="ls -hN --color=auto --group-directories-first"
+alias ls="ls -ahN --color=auto --group-directories-first"
 alias grep="grep --color=auto"
 alias diff="diff --color=auto"
+
+alias l1="ls -1"
+alias ll="ls -alh"
+
+
+function cdg() {
+    cd "$(git rev-parse --show-toplevel)"
+}
 
 # awp is an AWS_PROFILE picker
 function awp() {
@@ -23,32 +32,37 @@ function awp() {
     export AWS_PROFILE=$profile
 } 
 
-# kc is a per shell-scoped kubernetes context switcher
-#
-# It fuzzy selects a context from the default kubeconfig ($HOME/.kube/config)
-# and creates a new config with current-context set to the selected context.
-# The new config is saved in a temporary file bound by naming convention to the
-# particular shell PID (/tmp/kc_$$)
-#
-# The shell-specific config should be automatically detected and exported
-# under KUBECONFIG as part of the shell's precmd hook.
-# 
-# In ~/.zshrc:
-#   function kc_export() {
-#       [ -e "/tmp/kc_$$" ] && export KUBECONFIG=/tmp/kc_$$
-#   }
-#   typeset -a precmd_functions
-#   precmd_functions+=(kc_export)
-#
-#   # kc cleanup
-#   kc_trap_exit() {
-#     [ -e "/tmp/kc_$$" ] && rm /tmp/kc_$$ > /dev/null
-#   }
-#   trap kc_trap_exit EXIT
-#
-function kc() {
-    fzf_args=("--layout=reverse" "--height=10")
-    base="$HOME/.kube/config"
-    choice=$(yq '.contexts[] | .name' $base | fzf "${fzf_args[@]}")
-    yq ".current-context = \"$choice\"" $base > /tmp/kc_$$
+# derive_aws_profile sets and exports the AWS_PROFILE variable
+# based on current directory
+function derive_dir_aws_profile() {
+    case $(pwd) in
+        *"dev")
+            export AWS_PROFILE=dev-admin
+            ;;
+        *"stag")
+            export AWS_PROFILE=stag-admin
+            ;;
+        *"prod")
+            export AWS_PROFILE=prod-admin
+            ;;
+        *"shrd")
+            export AWS_PROFILE=shrd-admin
+            ;;
+    esac
 }
+# execute derive_dir_aws_profile on cd
+chpwd_functions+=(derive_dir_aws_profile)
+
+
+function dotsource() {
+    prefix="$HOME/repos/github.com/valorl/dotfiles/home/"
+    file=$(find $prefix \
+        -type f \( -name "*.zsh" -o -name "*.sh" -o -name ".zshrc" -o -name ".zshenv" \) \
+        | xargs -I{} bash -c "in={}; echo "\${in}" \${in#$prefix}" \
+        | fzf --with-nth=2 | cut -d' ' -f2)
+
+    source "$HOME/$file"
+    echo "✅ sourced $file"
+}
+
+alias dots=dotsource
